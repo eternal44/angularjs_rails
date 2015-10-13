8868a57a9d435e8320f2bf7030d46508ee7bed99
James Youn <jamesyoun710@gmail.com>
2015-10-13 16:19:06 -0700

    Gem & assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angularails::Application.config.secret_key_base = 'c3aef735d1ff64f32c4706db2ea787eb3cd91abd9b9956ff35301ea486ba0e1e7c67c01606a5587ab07684168de85f8dac7e99259c1287eae659263f1a6b7fc4'
